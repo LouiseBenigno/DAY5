7db1dd6c447870023b4b86d90556ba9a85feffe0
LouiseBenigno <benignolouise2@gmail.com>
2026-06-17 13:20:58 +0800

    Add files via upload

---
+++ Day5/DAY5/JOURNAL/journal.sql
@@ -0,0 +1,17 @@
+--JOURNAL
+
+-- 16. I am now more comfortable writing WHERE clauses, but I still get a bit confused when there are many conditions.
+-- The trickiest part for me is using AND and OR correctly because it can change the results if I mix them up.
+
+-- 17. The operator I find most useful is LIKE because it helps me search for patterns, especially when the data is not exact.
+-- It is very helpful for finding names or text.
+
+-- 18. During the pair activity, the collaboration was good.
+-- We shared ideas, and my partner helped me understand some parts I found difficult, especially how to use conditions correctly.
+
+-- 19. SQL filtering is a bit harder for me right now compared to Excel because Excel is more visual and easier to use.
+-- But I can see that SQL is more powerful and flexible.
+
+-- 20. One business question I want to answer using SQL is:
+-- "Which products sell the most each month?"
+-- This can help a business know what products to focus on or promote.
